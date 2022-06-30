e700224a767a75b4735ecf37618d99c68ca65528
Rai-Manish14 <manishrai14101999@gmail.com>
2022-06-30 15:45:03 +0530

    Changes at 30-06-2022

---
+++ audit_authorization/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Manish Kumar Rai', 'Manish', 'mkr1234', 'Project 1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Shanmukha Tuttagunta', 'Shanmukha', 'st1234', 'Project 2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Trina Das', 'Trina', 'td1234', 'Project 3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Anumolu Tejasri', 'Anumolu', 'at1234', 'Project 4');
